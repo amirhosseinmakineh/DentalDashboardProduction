0d253e52b5ef608ee20b92b8cdff8fa959acb7b6
amirhosseinmakineh <amirhosseinmakineh1379@gmail.com>
2026-06-16 10:01:45 -0700

    seconf commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,17 @@
+import { DashboardLayoutComponent } from './components/dashboard-layout.component/dashboard-layout.component.component';
+import { ConsultantComponent } from './components/dashboards/consultant/consultant.component';
 import { Routes } from '@angular/router';
-import { DashboardLayoutComponent } from './dashboard/components/layout/dashboard-layout.component';
-import { ConsultantComponent } from './dashboard/components/consultant/consultant.component';
-import { ConsultantAttendanceManagmentComponent } from './dashboard/components/consultant-attendance-managment/consultant-attendance-managment.component';
-import { ConsultantLeadManagmentComponent } from './dashboard/components/consultant-lead-managment/consultant-lead-managment.component';
 import { ConsultantMainComponent } from './dashboard/components/consultant-main/consultant-main.component';
-import { ConsultantScoreLogManagmentComponent } from './dashboard/components/consultant-score-log-managment/consultant-score-log-managment.component';
-
+import { ConsultantLeadManagmentComponent } from './dashboard/components/consultant-lead-managment/consultant-lead-managment.component';
+import { ConsultantAttendanceManagmentComponent } from './components/dashboards/consultantAttendanceManagment/consultantAttendanceManagment.component';
+import { ConsultantScoreLogManagmentComponent } from './components/dashboards/consultantScoreLogManagment/consultantScoreLogManagment.component';
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'dashboard/consultant/main' },
   {
     path: 'dashboard',
     component: DashboardLayoutComponent,
     children: [
-      { path: 'userManagement', loadComponent: () => import('./dashboard/components/user-management/user-management.component').then((m) => m.UserManagementComponent) },
+      { path: 'userManagement', loadComponent: () => import('./components/dashboards/userManagement/userManagement.component').then((m) => m.UserManagementComponent) },
       { path: 'consultant', component: ConsultantComponent },
       { path: 'consultant/main', component: ConsultantMainComponent },
       { path: 'consultant/:profileId/leads', component: ConsultantLeadManagmentComponent },
